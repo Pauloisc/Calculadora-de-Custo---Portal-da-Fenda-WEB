a6d2be761e513c76a8c0a352329dd27284965b68
Paulo Italo <pauloitalo061@gmail.com>
2026-06-19 19:44:49 -0300

    Criando times no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
+import { useState } from 'react';
+import { personagens, TIER_RULES, cones } from './data/personagem';
+import { calcularCustoPersonagem, calcularCustoCone, calcularCustoTotal } from './utils/calculos';
+
+const timeInicial = [
+  { personagem: "Nada", eidolons: 0, cone: "Nada", sobreposicao: 1 },
+  { personagem: "Nada", eidolons: 0, cone: "Nada", sobreposicao: 1 },
+  { personagem: "Nada", eidolons: 0, cone: "Nada", sobreposicao: 1 },
+  { personagem: "Nada", eidolons: 0, cone: "Nada", sobreposicao: 1 }
+];
 
 function App() {
+  const [time1, setTime1] = useState(timeInicial);
+  const [time2, setTime2] = useState(timeInicial);
+  const [custoAdicional, setCustoAdicional] = useState(0);
+  let custoTotal = calcularCustoTotal(time1,time2,custoAdicional);
   return (
     <div className="app-container">
       <h1>Portal da Fenda - Calculadora de Custos</h1>
       <p>Em breve aqui a nossa calculadora de Honkai Star Rail!</p>
+      <p>O custo total é: {custoTotal}</p>
     </div>
   )
 }
 
 export default App
-
