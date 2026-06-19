ecf867094a7931e96a1d7861662b3793a2ab0865
Paulo Italo <pauloitalo061@gmail.com>
2026-06-19 15:27:32 -0300

    Começando o projeto do Portal da Fenda WEB

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Portal da Fenda - Calculadora de Custos</h1>
+      <p>Em breve aqui a nossa calculadora de Honkai Star Rail!</p>
+    </div>
+  )
+}
+
+export default App
+
