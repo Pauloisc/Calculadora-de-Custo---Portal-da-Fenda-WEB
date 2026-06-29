5a3a949b8fc010f49bb35ca7ab781d440ef65d8f
Paulo Italo <pauloitalo061@gmail.com>
2026-06-28 22:27:03 -0300

    Inserindo custo de cone T5 diferente para personagens do tier D, E e F

--- src/utils/calculos.js
+++ src/utils/calculos.js
@@ -4,16 +4,18 @@ export function calcularCustoPersonagem(nome, eidolons){
     const personagemEncontrado = personagens.find(p => p.nome === nome);
     if (!personagemEncontrado) {return 0;}
     const regras = TIER_RULES[personagemEncontrado.tier];
-    const custo = (regras.base + regras.eidolon * eidolons);
-    return custo;
+    return (regras.base + regras.eidolon * eidolons)
 }
 
 export function calcularCustoCone(nomeCone, sobreposicao, nomePersonagem){
     if (nomePersonagem === "Nada" || !nomePersonagem) { return 0; }
     const regrasCone = cones[nomeCone];
     if (!regrasCone) {return 0;}
-    const custo = regrasCone.base + regrasCone.adicional * (sobreposicao - 1);
-    return custo;
+    const personagemEncontrado = personagens.find(p => p.nome === nomePersonagem);
+    if (personagemEncontrado && (personagemEncontrado.tier === "D" || personagemEncontrado.tier === "E" || personagemEncontrado.tier === "F")) {
+        if (nomeCone === "Cone T5"){
+            return 0.5 + regrasCone.adicional * (sobreposicao - 1);}}
+    return (regrasCone.base + regrasCone.adicional * (sobreposicao - 1));
 }
 
 export function calcularCustoTime(time){
@@ -28,10 +30,7 @@ export function calcularCustoTime(time){
         const dadosPersonagem = personagens.find(p => p.nome === slot.personagem);
         if (dadosPersonagem && dadosPersonagem.partner && dadosPersonagem.partner !== "Nada") {
             const parceiroPresente = time.some(s => s.personagem === dadosPersonagem.partner);
-            if (parceiroPresente) {
-                custoTime += 1;
-            }
-        }
+            if (parceiroPresente) {custoTime += 1;}}
     }
     return custoTime;
 }
